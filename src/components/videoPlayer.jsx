af55dbc92bcbfee700e38ce129ceb4013e769531
Aman Sharma <aman2062004@gmail.com>
2025-07-10 01:05:28 +0530

    Update videoPlayer.jsx

--- src/components/videoPlayer.jsx
+++ src/components/videoPlayer.jsx
@@ -4,16 +4,19 @@ export const VideoPlayer = ({ user }) => {
   const ref = useRef();
 
   useEffect(() => {
-    user.videoTrack.play(ref.current);
-  }, []);
+    if (user.videoTrack) {
+      user.videoTrack.play(ref.current);
+    }
+  }, [user.videoTrack]);
 
   return (
-    <div>
-      Uid: {user.uid}
-      <div
-        ref={ref}
-        style={{ width: '200px', height: '200px' }}
-      ></div>
-    </div>
+    <div
+      ref={ref}
+      style={{
+        width: '200px',
+        height: '150px',
+        backgroundColor: '#000',
+      }}
+    />
   );
 };
